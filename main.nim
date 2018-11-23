175461bd78adaec7e434835471e0aca476fbff44
Serialdev <carlos.mariscal.melgar@gmail.com>
2018-11-23 11:38:34 +0200

    [+] Added the Kahan algorithm for compensated summation of a sequence of floating point numbers

--- main.nim
+++ main.nim
@@ -1,4 +1,4 @@
-proc NeumaierSum( input:openArray[float] ): float =
+proc neumaierSum( input:openArray[float] ): float =
   var sum = input[0]
   var c = 0.0   # A running compensation for lost low-order bits
   for i in 1 ..< input.len:
@@ -9,5 +9,16 @@ proc NeumaierSum( input:openArray[float] ): float =
       c += (input[i] - t) + sum # Else low-order digits of sum are lost
     sum = t
   return sum + c  # Correction is only applied at the very end
+
+
+proc kahanSum(input:openArray[float]): float =
+  var sum = 0.0
+  var c = 0.0  # A runnng compensation for lost low-order bits
+  for i in 0 ..< input.len:
+    var y = input[i] - c    # c is zero
+    var t = sum + y         # sum ist big, y is small so low-order digits of y are lost
+    c = (t - sum) - y       # (t - sum) cancels the high-order part of y; substracting y recovers negative (low part of y)
+    sum = t                 # Algebraically, c should always be zero. Risk of loss of accuracy due to overly-agressive optimizing compilers!
+  return sum
 
 
