4c1ef0692f47cfa7222e83feea03ecb0d7077260
Serialdev <carlos.mariscal.melgar@gmail.com>
2019-03-22 10:33:35 +0200

    [*] Added some references

--- main.nim
+++ main.nim
@@ -1,3 +1,18 @@
+# ;;; inim.el --- Inim minor mode for Nim Repl support
+
+# ;; Copyright (C) 2018 Andres Mariscal
+
+# ;; Author: Andres Mariscal <carlos.mariscal.melgar@gmail.com>
+# ;; Created: 26 Sep 2018
+# ;; Version: 0.0.1
+# ;; Keywords: nim languages repl
+# ;; URL: https://github.com/serialdev/inim-mode
+# ;; Package-Requires: ((emacs "24.3", parsec ))
+# ;;; Commentary:
+# ;; Nim Repl support through inim repl
+
+# ;; Usage
+
 proc neumaierSum( input:openArray[float] ): float =
   var sum = input[0]
   var c = 0.0   # A running compensation for lost low-order bits
@@ -7,8 +22,8 @@ proc neumaierSum( input:openArray[float] ): float =
       c += (sum - t) + input[i] # If sum is bigger, low-order digits of input[i] are lost
     else:
       c += (input[i] - t) + sum # Else low-order digits of sum are lost
-    sum = t
-  return sum + c  # Correction is only applied at the very end
+    sum = t   # Correction is only applied at the very end
+  return sum + c
 
 
 proc kahanSum(input:openArray[float]): float =
@@ -21,4 +36,14 @@ proc kahanSum(input:openArray[float]): float =
     sum = t                 # Algebraically, c should always be zero. Risk of loss of accuracy due to overly-agressive optimizing compilers!
   return sum
 
+let numbers:seq[float64] = @[1.2 ,2.0,3.0,4.0,5.0]
 
+numbers.echo()
+neumaierSum(numbers).echo
+kahanSum(numbers).echo
+
+
+let zc = [[[1,2,3], [4,5,6] ],
+          [[11,22,33], [44,55,66]],
+          [[111,222,333], [444,555,666]   ],
+          [[1111,2222,3333],[4444,5555,6666]] ].toTensor()
